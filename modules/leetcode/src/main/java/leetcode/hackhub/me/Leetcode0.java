fa82e89f1841fb335b207f70ea616ddc134c1b99
mensondzh <cva.engineer.ding@gmail.com>
2021-08-28 22:16:34 +0800

    init

---
+++ modules/leetcode/src/main/java/leetcode/hackhub/me/Leetcode0.java
@@ -0,0 +1,14 @@
+package leetcode.hackhub.me;
+
+/**
+ * @Author mesondzh
+ * @Description: leetcode.hackhub.me
+ * @Date 2021-08-28 22:03
+ * @Copyright © 2017~2021 by hackhub.me
+ */
+
+public class Leetcode0 {
+    public static void main(String[] args) {
+        System.out.println("hello, leetcode.");
+    }
+}
